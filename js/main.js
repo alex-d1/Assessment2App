3785c132c47e71aecc79452c72f5903c9f906b24
alex-d1 <98593387+alex-d1@users.noreply.github.com>
2023-12-14 21:40:49 +1100

    mostly finished

--- js/main.js
+++ js/main.js
@@ -1,56 +1,39 @@
-
-// add event listener for initial layout
-// msnry.on( 'layoutComplete', function( items ) {
-//   msnry.layout();
-// });
-// trigger initial layout
-
-
+/** Find elements in document */
 const searchBox = document.getElementById('searchBox')
 const searchButton = document.getElementById('searchButton')
 const errorMessage = document.getElementById('errorMessage')
 const imageBox = document.getElementById('imageBox')
 const mapContainer = document.getElementById('mapContainer')
-const errorBox = document.getElementById("error"); // Find the P element with the ID of error
+const errorBox = document.getElementById("error");
 const pageLoading = document.getElementById("loadingSpinner"); // Loading spinner displayed when api is fetching
-// window.addEventListener('load', searchCityData) // Adds an event listener to the window that detects the load event
 
-// this function returns a promise that resolves after n milliseconds
-const wait = (n) => new Promise((resolve) => setTimeout(resolve, n));
+/** This function is used to pause operations to let things load */
+const wait = (n) => new Promise((resolve) => setTimeout(resolve, n)); 
 
-searchButton.addEventListener("click", search) //Add click event listener to search button
+/** Add click event listener to search button */
+searchButton.addEventListener("click", search) 
 
-const pixaBaseURL = 'https://pixabay.com/api/'
-const pixaApiKey = '40691540-3e797e9dfb04505d14334f2aa'
+const pixaBaseURL = 'https://pixabay.com/api/' //Stores the pixabay api url
+const pixaApiKey = '40691540-3e797e9dfb04505d14334f2aa' //Stores the api key
 
-const geoBaseURL = ''
-const geoAPIKey = 'qTKvlimnn76FUoDd227QGneOana1wTZWf_ehfEd9bYM'
+const geoBaseURL = 'https://geocode.search.hereapi.com/v1/geocode'
+const geoAPIKey = 'qTKvlimnn76FUoDd227QGneOana1wTZWf_ehfEd9bYM' //Stores the here api key
 
-const weatherBaseURL = ''
-const weatherAPIKey = 'fb879606d40b4356bc0535afa14c649d'
-
-
+const weatherBaseURL = 'https://api.weatherbit.io/v2.0/'
+const weatherAPIKey = 'fb879606d40b4356bc0535afa14c649d' //Stores the weatherkit api key
 
 
-/**
- * Moves the map to display over Berlin
- *
- * @param  {H.Map} map      A HERE Map instance within the application
- */
+
+
 
   
-  /**
-   * Boilerplate map initialization code starts below:
-   */
-  
-  //Step 1: initialize communication with the platform
-  // In your own code, replace variable window.apikey with your own apikey
+  // new here map object
   var platform = new H.service.Platform({
-    apikey: 'qTKvlimnn76FUoDd227QGneOana1wTZWf_ehfEd9bYM'
+    apikey: `${geoAPIKey}` //here map api key
   });
-  var defaultLayers = platform.createDefaultLayers();
+  var defaultLayers = platform.createDefaultLayers(); //load map layers
   
-  //Step 2: initialize a map - this map is centered over Europe
+  // initialize the map object
   var map = new H.Map(document.getElementById('cityMap'),
     defaultLayers.vector.normal.map,{
     center: {lat:0, lng:0},
@@ -60,9 +43,7 @@ const weatherAPIKey = 'fb879606d40b4356bc0535afa14c649d'
   // add a resize listener to make sure that the map occupies the whole container
   window.addEventListener('resize', () => map.getViewPort().resize());
   
-  //Step 3: make the map interactive
-  // MapEvents enables the event system
-  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+  //enables map interaction
   var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
   
   // Create the default UI components
@@ -71,80 +52,58 @@ const weatherAPIKey = 'fb879606d40b4356bc0535afa14c649d'
   // Get an instance of the search service:
   var service = platform.getSearchService();
 
-  // Call the "autosuggest" method with the search parameters,
-  // the callback and an error callback function (called if a
-  // communication error occurs):
-  service.autosuggest({
-    // Search query
-    q: 'Chicago ORD',
-    // Center of the search context
-    at: '38.71014896078624,-98.60787954719035'
-  }, (result) => {
-    let {position, title} = result.items[0];
-    // Assumption: ui is instantiated
-    // Create an InfoBubble at the returned location
-    ui.addBubble(new H.ui.InfoBubble(position, {
-      content: title
-    }));
-  }, alert);
-  
 
-//   END HERE API
 
 // The function that attempts to validate the text entry, starts the APIs and changes the visuals on the page
 function search(){
   // Very basic textbox validation
   if (searchBox.value == "") {
-    
-    errorBox.innerHTML = "The search box is empty!"
-    return
+    errorBox.innerHTML = "The search box is empty!" //displays message on page
+    return //stops function
   } else {
-    errorBox.innerHTML = ""
-    document.getElementById("headerText").classList.add("searchClicked")
-    document.getElementById("headerText").onanimationend = () => {document.getElementById("headerH").style.display='none'}
-    pageLoading.classList.add("loading")
-    const query = searchBox.value
-    searchCityData(query)
+    errorBox.innerHTML = "" //clears any error message
+    document.getElementById("headerText").classList.add("searchClicked") //applies class to a container when the search button is clicked
+    document.getElementById("headerText").onanimationend = () => {document.getElementById("headerH").style.display='none'} //removes the h1 from display completely when animation ends
+    pageLoading.classList.add("loading") //adds visibility class to a loading spinner
+    const query = searchBox.value //query is equal to the text typed in search box
+    searchCityData(query) //run function with query value
   }
 }
 
-//Call to the API's and display the information
+//Call to the API's and display the information, this is the main function of the app
 async function searchCityData(query) {
     //Try and run the following code
     try {
 
-        
-        
-        //find item
-        let geo = await fetch(`https://geocode.search.hereapi.com/v1/geocode?limit=20&q=${query}&apiKey=${geoAPIKey}`)
+        let geo = await fetch(`${geoBaseURL}?limit=20&q=${query}&apiKey=${geoAPIKey}`) //geo fetches the here api information
 
-        let photo = await fetch(`${pixaBaseURL}?key=${pixaApiKey}&q=${query}&image_type=photo&per_page=40`)
-        const data = {images:await photo.json(), location:await geo.json()} //Get a list of random facts from the response
-        displayData(data) //Pass the list of random facts to the displayData function
-        const lat = data.location.items[0].position.lat
-        console.log(lat)
-        const lon = data.location.items[0].position.lng
-        console.log(lon)
-        const address = data.location.items[0].address.city
-        let weather = await fetch(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${weatherAPIKey}`)
-        const cityWeatherData = {forecast:await weather.json()}
-        console.log(cityWeatherData)
-        moveMap(map,lat,lon)
-        
-        landmarkData(lat,lon)
-        displayWeatherData(cityWeatherData)
+        let photo = await fetch(`${pixaBaseURL}?key=${pixaApiKey}&q=${query}&image_type=photo&per_page=40`) //photo fetches the pixabay api information
+        const data = {images:await photo.json(), location:await geo.json()} //data object that contains the geo and photo responses, converted to json format
+        displayData(data) //pass the here and pixabay api information off to be displayed
 
-        await wait(10000)
-        mapContainer.classList.remove("visually-hidden")
-        await wait(3000)
-        pageLoading.classList.remove("loading")
-        map.getViewPort().resize()
-        await wait(3000)
-        document.getElementById("weatherCard").style.opacity='100'
+        const lat = data.location.items[0].position.lat //store latitude value from here api json result
+        const lon = data.location.items[0].position.lng //store longitude value from here api json result
+
+        let weather = await fetch(`${weatherBaseURL}current?lat=${lat}&lon=${lon}&key=${weatherAPIKey}`) //fetche weatherbit api results for city and store in weather variable
+        const cityWeatherData = {forecast:await weather.json()} //convert weather api result to json format
+
+        moveMap(map,lat,lon) //change location of here api map object to the city
+        
+        landmarkData(lat,lon) //send city coordinates to the landmark search function
+
+        displayWeatherData(cityWeatherData) //send city weather to the display weather function
+
+        await wait(10000) //apply a wait of 10 seconds to the function, this allows pictures to load before the map opens
+        mapContainer.classList.remove("visually-hidden") //removes the hide class from the map
+        await wait(3000) //3 second wait to make sure animations finish
+        pageLoading.classList.remove("loading") //hide the loading spinner
+        map.getViewPort().resize() //now that the map is visible, size to the new viewport 
+        await wait(3000) //wait 3 seconds
+        document.getElementById("weatherCard").style.opacity='100' //makes the weather card visible
         
         
     }
-    //Catch the error if it the code in the try block fails
+    //Catch the error if the code in the try block fails
     catch (error) {
         console.log(error); //Output the error to the console
         errorBox.innerHTML = "Sorry there's an error! 🤕" // Displays a error message
@@ -153,8 +112,8 @@ async function searchCityData(query) {
 
 //Moves the map to the city typed in the search bar
 function moveMap(map,lat,lon){
-  map.setCenter({lat:lat, lng:lon});
-  map.setZoom(12);
+  map.setCenter({lat:lat, lng:lon}); //map center on lat and lon
+  map.setZoom(12); //zooms map in
 }
 
 
@@ -163,56 +122,57 @@ function moveMap(map,lat,lon){
 //Parameters: Data - The data to display from the API
 async function displayData(data) {
   
-    //Loop over the array of random facts 
+    //Loop over the array of images
 
     for (var i = 0; i < data.images.hits.length; i++) {
         let col = document.createElement("div") //Create a div element to display the data
-        col.classList.add("col-3", "p-0")
-        col.innerHTML = `<img src="${data.images.hits[i].largeImageURL}"></img>` //Add the first fact to the p element
-        imageBox.appendChild(col); //Append the p element to the facts div on the page
-        await wait(500)
+        col.classList.add("col-3", "p-0") //adds bootstrap classes to the div, makes it a flexbox column
+        col.innerHTML = `<img src="${data.images.hits[i].largeImageURL}"></img>` //adds image element from pixabay to the div
+        imageBox.appendChild(col); //Append the div element to the page
+        await wait(500) //wait 5 seconds
         
+        // this is the masonry object that makes sure the images don't leave gaps no matter what size they are
+        // (putting it here seems wrong but it works)
         var msnry = new Masonry( '#imageBox', {
           percentPosition: true,
-          // disable initial layout
-          
         });
-      
     }
     
 }
 
+//Finds landmarks around lat and lon values
+//Parameters: lat - latitude value from here api geocode search
+//lon - longitude value from here api geocode search
+//(I couldn't really get this to work properly, the documentation for the here api is atrocious)
 async function landmarkData(lat,lon) {
-  // let landmarks = await fetch(`https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?apiKey=${geoAPIKey}&mode=retrieveLandmarks&prox=${lat},${lon},1000`)
-  // console.log(landmarks)
 
-  // Call the reverse geocode method with the geocoding parameters,
-// the callback and an error callback function (called if a
-// communication error occurs):
-// service.reverseGeocode({
-//   in: `circle,${lat},${lon},1000`,
-//   mode: 'retrieveLandmarks' 
-// }, (result) => {
-//   console.log(result)
-//   result.items.forEach((item) => {
-//     // Assumption: ui is instantiated
-//     // Create an InfoBubble at the returned location with
-//     // the address as its contents:
-//     ui.addBubble(new H.ui.InfoBubble(item.position, {
-//       content: item.address.label
-//     }));
-//   });
-// }, alert);
+//calls the reverse geocode method on the service object from the here api, declared above
+service.reverseGeocode({
+  at: `${lat},${lon},1000`, 
+  mode: 'retrieveLandmarks' 
+}, (result) => {
+  result.items.forEach((item) => {
+    // Assumption: ui is instantiated
+    // Create an InfoBubble at the returned location with
+    // the address as its contents:
+    ui.addBubble(new H.ui.InfoBubble(item.position, {
+      content: item.address.label
+    }));
+  });
+}, alert);
 }
 
+
+//Finds the page elements for the weather data and sends it to the dom
+//Parameters: cityWeatherData - the json response from weatherbit api
 async function displayWeatherData(cityWeatherData) {
   const wCityName = document.getElementById("wCityName")
   wCityName.innerHTML = cityWeatherData.forecast.data[0].city_name
   
   const wCityTime = document.getElementById("wCityTime")
   const cTimeDate = cityWeatherData.forecast.data[0].ob_time
-  const convertedTime = cTimeDate.split(" ")
-  wCityTime.innerHTML = convertedTime[1]
+  const convertedTime = cTimeDate.split(" ") //the string has to be split because it includes the date but we only want the time
+  wCityTime.innerHTML = convertedTime[1] //date is [0] and time is [1] in the array
 
   const wCityTemp = document.getElementById("wCityTemp")
   wCityTemp.innerHTML = `${cityWeatherData.forecast.data[0].app_temp}°C`
@@ -227,5 +187,5 @@ async function displayWeatherData(cityWeatherData) {
   wCityHumid.innerHTML = `${cityWeatherData.forecast.data[0].rh}%`
 
   const wCityIcon = document.getElementById("wCityIcon")
-  wCityIcon.innerHTML = `<img src="../images/w_icons/${cityWeatherData.forecast.data[0].weather.icon}.png"></img>`
+  wCityIcon.innerHTML = `<img src="../images/w_icons/${cityWeatherData.forecast.data[0].weather.icon}.png"></img>` //the weatherbit api has codes for icons that display the weather conditions, this is how it is displayed
 }
